c3b47b3509124249020a1812ab069fd80c4319d2
Mihray <2636853047@qq.com>
2022-08-25 20:56:37 +0800

    Create InputClick.vue

---
+++ src/components/InputClick.vue
@@ -0,0 +1,53 @@
+<template id="inputclick">
+  <div id="inputclick" class="inputclick">
+      <from action="">
+        <input type="text" v-model.lazy="message">
+        <button v-on:click="inputclick">submit</button>
+      </from>
+  </div>
+  <span>{{message}}</span>
+</template>
+<script>
+export default {
+  el:'inputclick',
+    data(){
+        return{
+            message:'',
+            inputmessage:''
+        }
+    },
+  methods: {
+    inputclick(){
+        
+        console.log('button')
+    }
+  },
+}
+</script>
+<style scoped>
+.inputclick{
+        display: flex;
+        justify-content: center;
+        /* align-items: center; */
+}
+.inputclick input{
+    outline:none;
+    /* position: absolute; */
+    margin-top: 50px;
+    margin-right: 10px;
+    /* display:block; */
+    width: 200px;
+    border-top:none ;
+    border-right: none;
+    border-left: none;
+    background-color: rgb(240,240,240);
+    border-bottom: 3px dashed rgb(251,182,177);
+}
+.inputclick button{
+    margin-top: 50px;
+    width: 50px;
+    height:25px;
+    border: 1px solid rgb(64,37,40);
+    box-shadow: 2px 2px rgb(64,37,40); 
+}
+</style>
